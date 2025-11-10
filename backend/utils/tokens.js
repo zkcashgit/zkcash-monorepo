95cd6c32af94f794ead882e135ffba2734dfb3e9
ZKCash <mild10966@gmail.com>
2025-11-10 12:59:49 +0000

    Create tokens.js

---
+++ backend/utils/tokens.js
@@ -0,0 +1,9 @@
+
+module.exports.FALLBACK = [
+  // SOL (native)
+  { symbol: "SOL", name: "Solana", mint: "So11111111111111111111111111111111111111112", decimals: 9 },
+  // USDC (Mainnet)
+  { symbol: "USDC", name: "USD Coin", mint: "EPjFWdd5AufqSSqeM2qN1xzybapC8G4wEGGkZwyTDt", decimals: 6 },
+  // USDT
+  { symbol: "USDT", name: "Tether USD", mint: "Es9vMFrzaCERZ6k6pG7G1Hh9u1Cq6qj7pG1j1iGkXUz", decimals: 6 }
+];
